0d40df954ee8cea1775a122e93a17d18e9f2dba5
[Kacper Pudelko] <[kacper.pudelko.kpk@gmail.com]>
2025-03-04 12:51:25 +0100

    dodanie zmiennych

--- homework 1/app.js
+++ homework 1/app.js
@@ -1,1 +1,11 @@
-console.log(2+1)
+let arrowLeft = document.querySelector('.arrow-left');
+console.log(arrowLeft);
+
+let arrowRight = document.querySelector('.arrow-right');
+console.log(arrowRight);
+
+let dot1 = document.querySelector('#dot1');
+
+let dot2 = document.querySelector('#dot2');
+
+let dot3 = document.querySelector('#dot3');
