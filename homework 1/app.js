8a89705a9d0ccba24e85a0c388769d7accf62715
[Kacper Pudelko] <[kacper.pudelko.kpk@gmail.com]>
2025-03-04 13:07:53 +0100

    refactoring

--- homework 1/app.js
+++ homework 1/app.js
@@ -6,28 +6,35 @@ let dot1 = document.querySelector('#dot1');
 let dot2 = document.querySelector('#dot2');
 let dot3 = document.querySelector('#dot3');
 
-
 let slide1 = document.querySelector('#slide1');
 let slide2 = document.querySelector('#slide2');
 let slide3 = document.querySelector('#slide3');
 
-let showSlide1 =() => {
+let hideActiveSlide = () => {
     let activeElement = document.querySelector('.active');
-activeElement.classList.remove('active');
-    slide1.classList.add('active');
+    activeElement.classList.remove('active');
+};
+
+let showSlide = (slideNumber) => {
+    hideActiveSlide();
+document.querySelector('#slide'+slideNumber).classList.add('active');
+};
+
+let showSlide1 =() => {
+ 
+    showSlide(1)
 };
 
 
 let showSlide2 =() => {
-    let activeElement = document.querySelector('.active');
-    activeElement.classList.remove('active');
-    slide2.classList.add('active');
+   
+    showSlide(2)
 };
 let showSlide3 =() => {
-    let activeElement = document.querySelector('.active');
-activeElement.classList.remove('active');
-    slide3.classList.add('active');
+  
+    showSlide(3)
 };
+
 dot1.addEventListener('click',showSlide1);
 dot2.addEventListener('click',showSlide2);
 dot3.addEventListener('click',showSlide3);
